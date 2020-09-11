4dbe763a9b625e0f02b0b19a3a066428f76f8b10
Mariem-ro <66451325+Mariem-ro@users.noreply.github.com>
2020-09-12 00:08:20 +0100

    Update my.js

--- my.js
+++ my.js
@@ -7,4 +7,23 @@ fetch(url)
     document.getElementById("app").textContent = d[0].status;
 
 });}
+
+function addGS(){
+    const url="https://script.google.com/macros/s/AKfycbzvTyHJsz-xCeosO2lRq2K2lqAFfcJear3704qAqxY-lT8kEFW5/exec";
+fetch(url,{
+    method: 'POST',
+    mode: 'no-cors',
+    cache: 'no-cache',
+    //credentials:'omit',
+    headers: {
+        'Content-Type':'application/json'
+
+   },
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer',
+    body: JSON.stringify({name:"olivia"})
+})
+
+;}
+document.getElementById("btn2").addEventListener("click",addGS);
 document.getElementById("btn").addEventListener("click",testGS);
